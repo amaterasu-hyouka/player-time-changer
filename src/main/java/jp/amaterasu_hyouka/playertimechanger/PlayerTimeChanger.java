f2c52d1c803a34f1a2d3f7f25c28b78a3a563d0c
amaterasu_hyouka <amaterasu.hyouka1218@gmail.com>
2026-03-10 04:18:22 +0900

    feat: Minecraftを依存を設定

---
+++ src/main/java/jp/amaterasu_hyouka/playertimechanger/PlayerTimeChanger.java
@@ -0,0 +1,16 @@
+package jp.amaterasu_hyouka.playertimechanger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PlayerTimeChanger extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
