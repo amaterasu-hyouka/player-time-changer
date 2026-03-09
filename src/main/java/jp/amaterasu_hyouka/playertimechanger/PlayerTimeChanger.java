d43f4fd341b8866a909e5d7d99714627177a6316
amaterasu_hyouka <amaterasu.hyouka1218@gmail.com>
2026-03-10 04:19:45 +0900

    feat: ログの出力機能を追加

--- src/main/java/jp/amaterasu_hyouka/playertimechanger/PlayerTimeChanger.java
+++ src/main/java/jp/amaterasu_hyouka/playertimechanger/PlayerTimeChanger.java
@@ -4,13 +4,19 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PlayerTimeChanger extends JavaPlugin {
 
+    private static PlayerTimeChanger instance;
+    public static JavaPlugin getInstance(){
+        return instance;
+    }
+
     @Override
     public void onEnable() {
-
+        instance = this;
+        Log.info("PlayerTimeChangerを起動しました");
     }
 
     @Override
     public void onDisable() {
-
+        Log.info("PlayerTimeChangerを停止しました");
     }
 }
